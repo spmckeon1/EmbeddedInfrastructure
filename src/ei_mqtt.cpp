89060b8af9557ec54ce67869cf1b90e85778e447
Stephen McKeon <stephen@rsmckeon.com>
2026-07-26 16:15:49 -0500

    Add MQTT subsystem skeleton

---
+++ src/ei_mqtt.cpp
@@ -0,0 +1,8 @@
+//
+//  ei_mqtt.c
+//  
+//
+//  Created by Stephen McKeon on 7/26/26.
+//
+
+#include "ei_mqtt.h"
---
+++ src/ei_mqtt.h
@@ -0,0 +1,13 @@
+//
+//  ei_mqtt.h
+//  
+//
+//  Created by Stephen McKeon on 7/26/26.
+//
+
+#ifndef ei_mqtt_h
+#define ei_mqtt_h
+
+#include <stdio.h>
+
+#endif /* ei_mqtt_h */
